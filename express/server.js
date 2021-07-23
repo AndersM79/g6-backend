3670f983ea9beadb1afab9dcfc5300ab7f202c26
Anders79 <anderson.monroy.r79@gmail.com>
2021-07-22 21:05:21 -0500

    netrlyfy

--- express/server.js
+++ express/server.js
@@ -3,13 +3,15 @@ const path = require('path');
 const serverless = require('serverless-http');
 const bodyParser = require('body-parser');
 
+let productos = [{ name: "papitas" }, { name: "agua" }];
+
 const app = express();
  
 const router = express.Router();
 router.get('/', (req, res) => res.send({"hola": "hola"}));
+router.get('/productos', (req, res) => res.send(productos);
 
 app.use(bodyParser.json());
-
 app.use('/.netlify/functions/server', router);  // path must route to lambda
 
 module.exports = app;
